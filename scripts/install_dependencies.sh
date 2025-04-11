06043d6ca15dc1df7862768bed0fbb4950de00b6
csairam <sairamc91619@gmail.com>
2025-04-11 21:52:50 +0530

    intialcommit

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ec2-user/myapp
+npm install  # or pip install -r requirements.txt
---
+++ scripts/start_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ec2-user/myapp
+nohup node app.js &  # or python app.py
